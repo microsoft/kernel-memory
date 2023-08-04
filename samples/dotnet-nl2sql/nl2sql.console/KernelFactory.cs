140a8aa640e9bc6baf551f44333b6ace7c7a5e67
Chris Rickman <crickman@microsoft.com>
2023-08-03 21:55:46 -0700

    Added

---
+++ samples/dotnet-nl2sql/nl2sql.console/KernelFactory.cs
@@ -0,0 +1,127 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace SemanticKernel.Data.Nl2Sql;
+
+using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Memory;
+
+/// <summary>
+/// Responsible for initializing Semantic <see cref="Kernel"/> based on the configuration.
+/// </summary>
+internal static class KernelFactory
+{
+    // Azure settings
+    private const string SettingNameAzureApiKey = "AZURE_OPENAI_KEY";
+    private const string SettingNameAzureEndpoint = "AZURE_OPENAI_ENDPOINT";
+    private const string SettingNameAzureModelCompletion = "AZURE_OPENAI_DEPLOYMENT_NAME";
+    private const string SettingNameAzureModelEmbedding = "AZURE_OPENAI_EMBEDDINGS_DEPLOYMENT_NAME";
+
+    // Open AI settings
+    private const string SettingNameOpenAIApiKey = "OPENAI_API_KEY";
+    private const string SettingNameOpenAIModelCompletion = "OPENAI_API_COMPLETION_MODEL";
+    private const string SettingNameOpenAIModelEmbedding = "OPENAI_API_EMBEDDING_MODEL";
+
+    /// <summary>
+    /// Penalty for using any model less than GPT4 for SQL generation.
+    /// </summary>
+    private const string DefaultChatModel = "gpt-4";
+    private const string DefaultEmbedModel = "text-embedding-ada-003";
+
+    /// <summary>
+    /// Factory method for <see cref="IServiceCollection"/>
+    /// </summary>
+    public static Func<IServiceProvider, IKernel> Create(IConfiguration configuration)
+    {
+        return CreateKernel;
+
+        IKernel CreateKernel(IServiceProvider provider)
+        {
+            var logger = provider.GetService<ILogger<IKernel>>();
+
+            var apikey = configuration.GetValue<string>(SettingNameAzureApiKey);
+            if (!string.IsNullOrWhiteSpace(apikey))
+            {
+                var endpoint = configuration.GetValue<string>(SettingNameAzureEndpoint) ??
+                    throw new InvalidDataException($"No endpoint configured in {SettingNameAzureEndpoint}.");
+
+                var modelCompletion = configuration.GetValue<string>(SettingNameAzureModelCompletion);
+                var modelEmbedding = configuration.GetValue<string>(SettingNameAzureModelEmbedding);
+
+                return ConfigureAzure(endpoint, apikey, modelCompletion, modelEmbedding, logger).Build();
+            }
+
+            apikey = configuration.GetValue<string>(SettingNameOpenAIApiKey);
+            if (!string.IsNullOrWhiteSpace(apikey))
+            {
+                var modelCompletion = configuration.GetValue<string>(SettingNameOpenAIModelCompletion);
+                var modelEmbedding = configuration.GetValue<string>(SettingNameOpenAIModelEmbedding);
+
+                return ConfigureOpenAI(apikey, modelCompletion, modelEmbedding, logger).Build();
+            }
+
+            throw new InvalidDataException($"No api-key configured in {SettingNameAzureApiKey} or {SettingNameOpenAIApiKey}.");
+        }
+    }
+
+    private static KernelBuilder ConfigureOpenAI(
+        string apikey,
+        string? modelCompletion,
+        string? modelEmbedding,
+        ILogger<IKernel>? logger)
+    {
+        var builder = new KernelBuilder();
+
+        if (logger != null)
+        {
+            builder.WithLogger(logger);
+        }
+
+        modelCompletion ??= DefaultChatModel;
+        modelEmbedding ??= DefaultEmbedModel;
+
+        builder
+            .WithMemoryStorage(new VolatileMemoryStore())
+            .WithOpenAITextEmbeddingGenerationService(modelEmbedding, apikey);
+
+        if (!modelCompletion.StartsWith("gpt", StringComparison.OrdinalIgnoreCase))
+        {
+            return builder.WithOpenAITextCompletionService(modelCompletion, apikey);
+        }
+
+        return builder.WithOpenAIChatCompletionService(modelCompletion, apikey);
+    }
+
+    private static KernelBuilder ConfigureAzure(
+        string endpoint,
+        string apikey,
+        string? modelCompletion,
+        string? modelEmbedding,
+        ILogger<IKernel>? logger)
+    {
+        var builder = new KernelBuilder();
+
+        if (logger != null)
+        {
+            builder.WithLogger(logger);
+        }
+
+        modelCompletion ??= DefaultChatModel;
+        modelEmbedding ??= DefaultEmbedModel;
+
+        builder
+            .WithMemoryStorage(new VolatileMemoryStore())
+            .WithAzureTextEmbeddingGenerationService(modelEmbedding, endpoint, apikey);
+
+        if (!modelCompletion.StartsWith("gpt", StringComparison.OrdinalIgnoreCase))
+        {
+            return builder.WithAzureTextCompletionService(modelCompletion, endpoint, apikey);
+        }
+
+        return builder.WithAzureChatCompletionService(modelCompletion, endpoint, apikey);
+    }
+}
---
+++ samples/dotnet-nl2sql/nl2sql.library/Schema/ISchemaFormatter.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+namespace SemanticKernel.Data.Nl2Sql.Library.Schema;
+
+using System.IO;
+using System.Threading.Tasks;
+
+internal interface ISchemaFormatter
+{
+    Task WriteAsync(TextWriter writer, SchemaDefinition schema);
+}
