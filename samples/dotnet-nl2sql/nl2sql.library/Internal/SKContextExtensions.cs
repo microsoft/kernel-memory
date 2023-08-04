13d15c95e65d265d52024d7309098f2e877f6a48
Chris <66376200+crickman@users.noreply.github.com>
2023-08-03 22:25:33 -0700

    Added (#6)
    Sample code: Demonstrate abilities and limitations of LLM to generate SQL query.

---
+++ samples/dotnet-nl2sql/nl2sql.library/Internal/SKContextExtensions.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+namespace SemanticKernel.Data.Nl2Sql.Library.Internal;
+
+using System;
+using System.IO;
+using Microsoft.SemanticKernel.Orchestration;
+
+internal static class SKContextExtensions
+{
+    public static string GetResult(this SKContext context, string? label = null)
+    {
+        if (context == null)
+        {
+            return string.Empty;
+        }
+
+        if (context.ErrorOccurred)
+        {
+            if (context.LastException != null)
+            {
+                throw new InvalidDataException("No result available due to an unexpected failure.", context.LastException);
+            }
+
+            throw new InvalidDataException(context.LastErrorDescription);
+        }
+
+        var result = context.Result;
+
+        if (!string.IsNullOrWhiteSpace(label))
+        {
+            // Trim out label, if present.
+            int index = result.IndexOf($"{label}:", StringComparison.OrdinalIgnoreCase);
+            if (index > -1)
+            {
+                result = result.Substring(index + label.Length + 1);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ samples/dotnet-nl2sql/run.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+setlocal
+
+cd %~dp0/nl2sql.console
+
+dotnet restore
+dotnet build
+
+cls
+
+dotnet run
