b7484492fee648d4854cdb8d7622368e4e4315f7
Devis Lucato <devis@microsoft.com>
2023-09-28 13:42:04 -0700

    Add example about MVC integration

---
+++ examples/007-aspnet-mvc-integration/Controllers/MemoryController.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.SemanticMemory;
+
+namespace Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MemoryController : ControllerBase
+{
+    private readonly ISemanticMemoryClient _memory;
+
+    public MemoryController(ISemanticMemoryClient memory)
+    {
+        this._memory = memory;
+    }
+
+    [HttpGet(Name = "GetAnswer")]
+    public async Task<MemoryAnswer> GetAsync()
+    {
+        return await this._memory.AskAsync("What's Microsoft Copilot?");
+    }
+}
---
+++ examples/007-aspnet-mvc-integration/Program.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticMemory;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// === Memory stuff begin ===
+ISemanticMemoryClient memory = new MemoryClientBuilder()
+    .WithOpenAIDefaults(Env.Var("OPENAI_API_KEY"))
+    .Build();
+
+builder.Services.AddSingleton(memory);
+// === Memory stuff end ===
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
