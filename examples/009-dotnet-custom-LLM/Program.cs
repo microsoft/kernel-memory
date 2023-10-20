649fb21b01e9bd1d51831cde7ac3ce66339d02b0
Devis Lucato <devis@microsoft.com>
2023-10-20 12:37:50 -0700

    Add example about Llama and custom LLMs

---
+++ examples/009-dotnet-custom-LLM/Program.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Runtime.CompilerServices;
+using Microsoft.SemanticMemory;
+using Microsoft.SemanticMemory.AI;
+using Microsoft.SemanticMemory.MemoryStorage.Qdrant;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var llamaConfig = new LlamaConfig
+        {
+            // ...
+        };
+
+        var openAIConfig = new OpenAIConfig
+        {
+            EmbeddingModel = "text-embedding-ada-002",
+            APIKey = Env.Var("OPENAI_API_KEY")
+        };
+
+        var memory = new MemoryClientBuilder()
+            .WithCustomTextGeneration(new LlamaTextGeneration(llamaConfig))
+            .WithOpenAITextEmbedding(openAIConfig)
+            .WithQdrant(new QdrantConfig
+            {
+                /* ... */
+            });
+
+        // ...
+    }
+}
+
+public class LlamaConfig
+{
+    // ...
+}
+
+public class LlamaTextGeneration : ITextGeneration
+{
+    private readonly LlamaConfig _config;
+
+    public LlamaTextGeneration(LlamaConfig config)
+    {
+        this._config = config;
+    }
+
+    public async IAsyncEnumerable<string> GenerateTextAsync(
+        string prompt,
+        TextGenerationOptions options,
+        [EnumeratorCancellation] CancellationToken cancellationToken = new())
+    {
+        // ...
+
+        await Task.Delay(0, cancellationToken).ConfigureAwait(false);
+
+        yield return "some text";
+    }
+}
