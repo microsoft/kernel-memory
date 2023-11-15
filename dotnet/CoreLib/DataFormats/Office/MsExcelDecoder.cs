92a898a89e7638ae9f26a37390b8207da574bc01
Anthony Puppo <anthonyosx@gmail.com>
2023-11-15 18:59:44 -0500

    Add basic Excel text extraction (#104)
    ## Motivation and Context (Why the change? What's the scenario?)
    Adds support for extracting text content from Excel (.xlsx) documents.
    
    ## High level description (Approach, Design)
    Uses [ClosedXML](https://github.com/ClosedXML/ClosedXML) for text
    extraction.
    
    ---------
    
    Co-authored-by: Devis Lucato <devis@microsoft.com>

---
+++ dotnet/CoreLib/DataFormats/Office/MsExcelDecoder.cs
@@ -0,0 +1,115 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using ClosedXML.Excel;
+
+namespace Microsoft.KernelMemory.DataFormats.Office;
+
+public class MsExcelDecoder
+{
+    private const string DefaultSheetNumberTemplate = "\n# Worksheet {number}\n";
+    private const string DefaultEndOfSheetTemplate = "\n# End of worksheet {number}";
+    private const string DefaultRowPrefix = "";
+    private const string DefaultColumnSeparator = ", ";
+    private const string DefaultRowSuffix = "";
+
+    private readonly bool _withWorksheetNumber;
+    private readonly bool _withEndOfWorksheetMarker;
+    private readonly bool _withQuotes;
+    private readonly string _worksheetNumberTemplate;
+    private readonly string _endOfWorksheetMarkerTemplate;
+    private readonly string _rowPrefix;
+    private readonly string _columnSeparator;
+    private readonly string _rowSuffix;
+
+    public MsExcelDecoder(
+        bool withWorksheetNumber = true,
+        bool withEndOfWorksheetMarker = false,
+        bool withQuotes = true,
+        string? worksheetNumberTemplate = null,
+        string? endOfWorksheetMarkerTemplate = null,
+        string? rowPrefix = null,
+        string? columnSeparator = null,
+        string? rowSuffix = null)
+    {
+        this._withWorksheetNumber = withWorksheetNumber;
+        this._withEndOfWorksheetMarker = withEndOfWorksheetMarker;
+        this._withQuotes = withQuotes;
+
+        this._worksheetNumberTemplate = worksheetNumberTemplate ?? DefaultSheetNumberTemplate;
+        this._endOfWorksheetMarkerTemplate = endOfWorksheetMarkerTemplate ?? DefaultEndOfSheetTemplate;
+
+        this._rowPrefix = rowPrefix ?? DefaultRowPrefix;
+        this._columnSeparator = columnSeparator ?? DefaultColumnSeparator;
+        this._rowSuffix = rowSuffix ?? DefaultRowSuffix;
+    }
+
+    public string DocToText(string filename)
+    {
+        using var stream = File.OpenRead(filename);
+        return this.DocToText(stream);
+    }
+
+    public string DocToText(BinaryData data)
+    {
+        using var stream = data.ToStream();
+        return this.DocToText(stream);
+    }
+
+    public string DocToText(Stream data)
+    {
+        using var workbook = new XLWorkbook(data);
+        var sb = new StringBuilder();
+
+        var worksheetNumber = 0;
+        foreach (var worksheet in workbook.Worksheets)
+        {
+            worksheetNumber++;
+            if (this._withWorksheetNumber)
+            {
+                sb.AppendLine(this._worksheetNumberTemplate.Replace("{number}", $"{worksheetNumber}", StringComparison.OrdinalIgnoreCase));
+            }
+
+            foreach (IXLRangeRow? row in worksheet.RangeUsed().RowsUsed())
+            {
+                if (row == null) { continue; }
+
+                var cells = row.CellsUsed().ToList();
+
+                sb.Append(this._rowPrefix);
+                for (var i = 0; i < cells.Count; i++)
+                {
+                    IXLCell? cell = cells[i];
+
+                    if (this._withQuotes && cell is { Value.IsText: true })
+                    {
+                        sb.Append('"')
+                            .Append(cell.Value.GetText().Replace("\"", "\"\"", StringComparison.Ordinal))
+                            .Append('"');
+                    }
+                    else
+                    {
+                        sb.Append(cell.Value);
+                    }
+
+                    if (i < cells.Count - 1)
+                    {
+                        sb.Append(this._columnSeparator);
+                    }
+                }
+
+                sb.AppendLine(this._rowSuffix);
+            }
+
+            if (this._withEndOfWorksheetMarker)
+            {
+                sb.AppendLine(this._endOfWorksheetMarkerTemplate.Replace("{number}", $"{worksheetNumber}", StringComparison.OrdinalIgnoreCase));
+            }
+        }
+
+        return sb.ToString().Trim();
+    }
+}
--- dotnet/CoreLib/Handlers/TextExtractionHandler.cs
+++ dotnet/CoreLib/Handlers/TextExtractionHandler.cs
@@ -112,6 +112,15 @@ public class TextExtractionHandler : IPipelineStepHandler
 
                     break;
 
+                case MimeTypes.MsExcel:
+                    this._log.LogDebug("Extracting text from MS Excel file {0}", uploadedFile.Name);
+                    if (fileContent.ToArray().Length > 0)
+                    {
+                        text = new MsExcelDecoder().DocToText(fileContent);
+                    }
+
+                    break;
+
                 case MimeTypes.Pdf:
                     this._log.LogDebug("Extracting text from PDF file {0}", uploadedFile.Name);
                     if (fileContent.ToArray().Length > 0)
