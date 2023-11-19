38a1b9f17221f7534eb734b04b4b3f250a541788
Devis Lucato <devis@microsoft.com>
2023-11-18 20:09:13 -0800

    Restructure project files Add API key to functional tests Update some dependencies Reorganize examples

---
+++ examples/002-dotnet-SemanticKernelPlugin/Program.cs
@@ -0,0 +1,135 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.KernelMemory;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Orchestration;
+
+public static class Program
+{
+    // ReSharper disable InconsistentNaming
+    public static async Task Main()
+    {
+        const string Document = "mydocs-NASA-news.pdf";
+        const string Question1 = "any news about Orion?";
+        const string Question2 = "any news about Hubble telescope?";
+        const string Question3 = "what is a solar eclipse?";
+
+        // === PREPARE KERNEL ===
+        // Usual code to create an instance of SK, using Azure OpenAI.
+        // You can use any LLM, replacing `WithAzureChatCompletionService` with other LLM options.
+
+        var builder = new KernelBuilder();
+        builder.WithAzureOpenAIChatCompletionService(
+            deploymentName: EnvVar("AOAI_DEPLOYMENT_TEXT"),
+            endpoint: EnvVar("AOAI_ENDPOINT"),
+            apiKey: EnvVar("AOAI_API_KEY"));
+
+        var kernel = builder.Build();
+
+        // === PREPARE A SIMPLE SEMANTIC FUNCTION
+        // A simple prompt showing how you can leverage the memory inside prompts and semantic functions.
+        // See how "memory.ask" is used to pass the user question. At runtime the block is replaced with the
+        // answer provided by the memory service.
+
+        var skPrompt = """
+                       Question to Kernel Memory: {{$input}}
+
+                       Kernel Memory Answer: {{memory.ask $input}}
+
+                       If the answer is empty say 'I don't know' otherwise reply with a preview of the answer, truncated to 15 words.
+                       """;
+
+        var oracleFunction = kernel.CreateSemanticFunction(skPrompt);
+
+        // === PREPARE MEMORY PLUGIN ===
+        // Load the Kernel Memory plugin into Semantic Kernel.
+        // We're using a local instance here, so remember to start the service locally first,
+        // otherwise change the URL pointing to your KM endpoint.
+
+        var memoryConnector = GetMemoryConnector();
+        var memoryPlugin = kernel.ImportFunctions(new MemoryPlugin(memoryConnector), "memory");
+
+        // === LOAD DOCUMENT INTO MEMORY ===
+        // Load some data in memory, in this case use a PDF file, though
+        // you can also load web pages, Word docs, raw text, etc.
+
+        await memoryConnector.ImportDocumentAsync(Document, documentId: "NASA001");
+
+        // === RUN SEMANTIC FUNCTION ===
+        // Run some example questions, showing how the answer is grounded on the document uploaded.
+        // Only the first question can be answered, because the document uploaded doesn't contain any
+        // information about Question2 and Question3.
+
+        Console.WriteLine("---------");
+        Console.WriteLine(Question1 + "\n");
+        FunctionResult answer = await oracleFunction.InvokeAsync(Question1, kernel);
+        Console.WriteLine("Answer: " + answer);
+
+        Console.WriteLine("---------");
+        Console.WriteLine(Question2 + "\n");
+        answer = await oracleFunction.InvokeAsync(Question2, kernel);
+        Console.WriteLine("Answer: " + answer);
+
+        Console.WriteLine("---------");
+        Console.WriteLine(Question3 + "\n");
+        answer = await oracleFunction.InvokeAsync(Question3, kernel);
+        Console.WriteLine("Answer: " + answer);
+    }
+
+    /* ===== OUTPUT =====
+
+    ---------
+    any news about Orion?
+
+    Answer: Yes, NASA has invited media to see the new test version of the Orion spacecraft...
+    ---------
+    any news about Hubble telescope?
+
+    Answer: I don't know.
+    ---------
+    what is a solar eclipse?
+
+    Answer: I don't know.
+
+    */
+
+    private static IKernelMemory GetMemoryConnector(bool serverless = false)
+    {
+        if (!serverless)
+        {
+            return new MemoryWebClient("http://127.0.0.1:9001/", Environment.GetEnvironmentVariable("MEMORY_API_KEY"));
+        }
+
+        Console.WriteLine("This code is intentionally disabled.");
+        Console.WriteLine("To test the plugin with Serverless memory:");
+        Console.WriteLine("* Add a project reference to CoreLib");
+        Console.WriteLine("* Uncomment/edit the code in " + nameof(GetMemoryConnector));
+        Environment.Exit(-1);
+        return null;
+
+        // return new KernelMemoryBuilder()
+        //     .WithAzureOpenAIEmbeddingGeneration(new AzureOpenAIConfig
+        //     {
+        //         APIType = AzureOpenAIConfig.APITypes.EmbeddingGeneration,
+        //         Endpoint = EnvVar("AOAI_ENDPOINT"),
+        //         Deployment = EnvVar("AOAI_DEPLOYMENT_EMBEDDING"),
+        //         Auth = AzureOpenAIConfig.AuthTypes.APIKey,
+        //         APIKey = EnvVar("AOAI_API_KEY"),
+        //     })
+        //     .WithAzureOpenAITextGeneration(new AzureOpenAIConfig
+        //     {
+        //         APIType = AzureOpenAIConfig.APITypes.ChatCompletion,
+        //         Endpoint = EnvVar("AOAI_ENDPOINT"),
+        //         Deployment = EnvVar("AOAI_DEPLOYMENT_TEXT"),
+        //         Auth = AzureOpenAIConfig.AuthTypes.APIKey,
+        //         APIKey = EnvVar("AOAI_API_KEY"),
+        //     })
+        //     .BuildServerlessClient();
+    }
+
+    private static string EnvVar(string name)
+    {
+        return Environment.GetEnvironmentVariable(name)
+               ?? throw new ArgumentException($"Env var {name} not set");
+    }
+}
---
+++ service/Core/MemoryServerless.cs
@@ -0,0 +1,228 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.KernelMemory.Configuration;
+using Microsoft.KernelMemory.Diagnostics;
+using Microsoft.KernelMemory.Pipeline;
+using Microsoft.KernelMemory.Search;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.KernelMemory;
+
+/// <summary>
+/// Memory client to upload files and search for answers, without depending
+/// on a web service. By design this class is hardcoded to use
+/// <see cref="InProcessPipelineOrchestrator"/>, hence the name "Serverless".
+/// The class accesses directly storage, vectors and AI.
+/// </summary>
+public class MemoryServerless : IKernelMemory
+{
+    private readonly SearchClient _searchClient;
+    private readonly InProcessPipelineOrchestrator _orchestrator;
+
+    public InProcessPipelineOrchestrator Orchestrator => this._orchestrator;
+
+    public MemoryServerless(
+        InProcessPipelineOrchestrator orchestrator,
+        SearchClient searchClient)
+    {
+        this._orchestrator = orchestrator ?? throw new ConfigurationException("The orchestrator is NULL");
+        this._searchClient = searchClient ?? throw new ConfigurationException("The search client is NULL");
+    }
+
+    /// <summary>
+    /// Register a pipeline handler. If a handler for the same step name already exists, it gets replaced.
+    /// </summary>
+    /// <param name="handler">Handler instance</param>
+    public void AddHandler(IPipelineStepHandler handler)
+    {
+        this._orchestrator.AddHandler(handler);
+    }
+
+    /// <inheritdoc />
+    public Task<string> ImportDocumentAsync(
+        Document document,
+        string? index = null,
+        IEnumerable<string>? steps = null,
+        CancellationToken cancellationToken = default)
+    {
+        DocumentUploadRequest uploadRequest = new(document, index, steps);
+        return this.ImportDocumentAsync(uploadRequest, cancellationToken);
+    }
+
+    /// <inheritdoc />
+    public Task<string> ImportDocumentAsync(
+        string filePath,
+        string? documentId = null,
+        TagCollection? tags = null,
+        string? index = null,
+        IEnumerable<string>? steps = null,
+        CancellationToken cancellationToken = default)
+    {
+        var document = new Document(documentId, tags: tags).AddFile(filePath);
+        DocumentUploadRequest uploadRequest = new(document, index, steps);
+        return this.ImportDocumentAsync(uploadRequest, cancellationToken);
+    }
+
+    /// <inheritdoc />
+    public Task<string> ImportDocumentAsync(
+        DocumentUploadRequest uploadRequest,
+        CancellationToken cancellationToken = default)
+    {
+        var index = IndexExtensions.CleanName(uploadRequest.Index);
+        return this._orchestrator.ImportDocumentAsync(index, uploadRequest, cancellationToken);
+    }
+
+    /// <inheritdoc />
+    public Task<string> ImportDocumentAsync(
+        Stream content,
+        string? fileName = null,
+        string? documentId = null,
+        TagCollection? tags = null,
+        string? index = null,
+        IEnumerable<string>? steps = null,
+        CancellationToken cancellationToken = default)
+    {
+        var document = new Document(documentId, tags: tags).AddStream(fileName, content);
+        DocumentUploadRequest uploadRequest = new(document, index, steps);
+        return this.ImportDocumentAsync(uploadRequest, cancellationToken);
+    }
+
+    /// <inheritdoc />
+    public async Task<string> ImportTextAsync(
+        string text,
+        string? documentId = null,
+        TagCollection? tags = null,
+        string? index = null,
+        IEnumerable<string>? steps = null,
+        CancellationToken cancellationToken = default)
+    {
+        var content = new MemoryStream(Encoding.UTF8.GetBytes(text));
+        await using (content.ConfigureAwait(false))
+        {
+            return await this.ImportDocumentAsync(
+                content,
+                fileName: "content.txt",
+                documentId: documentId,
+                tags: tags,
+                index: index,
+                steps: steps,
+                cancellationToken: cancellationToken).ConfigureAwait(false);
+        }
+    }
+
+    /// <inheritdoc />
+    public async Task<string> ImportWebPageAsync(
+        string url,
+        string? documentId = null,
+        TagCollection? tags = null,
+        string? index = null,
+        IEnumerable<string>? steps = null,
+        CancellationToken cancellationToken = default)
+    {
+        var uri = new Uri(url);
+        Verify.ValidateUrl(uri.AbsoluteUri, requireHttps: false, allowReservedIp: false, allowQuery: true);
+
+        using Stream content = new MemoryStream(Encoding.UTF8.GetBytes(uri.AbsoluteUri));
+        return await this.ImportDocumentAsync(content, fileName: "content.url", documentId: documentId, tags: tags, index: index, steps: steps, cancellationToken: cancellationToken)
+            .ConfigureAwait(false);
+    }
+
+    /// <inheritdoc />
+    public async Task<IEnumerable<IndexDetails>> ListIndexesAsync(CancellationToken cancellationToken = default)
+    {
+        return (from index in await this._searchClient.ListIndexesAsync(cancellationToken).ConfigureAwait(false)
+                select new IndexDetails { Name = index });
+    }
+
+    /// <inheritdoc />
+    public Task DeleteIndexAsync(string? index = null, CancellationToken cancellationToken = default)
+    {
+        return this._orchestrator.StartIndexDeletionAsync(index: index, cancellationToken);
+    }
+
+    /// <inheritdoc />
+    public Task DeleteDocumentAsync(string documentId, string? index = null, CancellationToken cancellationToken = default)
+    {
+        return this._orchestrator.StartDocumentDeletionAsync(documentId: documentId, index: index, cancellationToken);
+    }
+
+    /// <inheritdoc />
+    public async Task<bool> IsDocumentReadyAsync(
+        string documentId,
+        string? index = null,
+        CancellationToken cancellationToken = default)
+    {
+        index = IndexExtensions.CleanName(index);
+        return await this._orchestrator.IsDocumentReadyAsync(index: index, documentId, cancellationToken).ConfigureAwait(false);
+    }
+
+    /// <inheritdoc />
+    public async Task<DataPipelineStatus?> GetDocumentStatusAsync(
+        string documentId,
+        string? index = null,
+        CancellationToken cancellationToken = default)
+    {
+        index = IndexExtensions.CleanName(index);
+        DataPipeline? pipeline = await this._orchestrator.ReadPipelineStatusAsync(index: index, documentId, cancellationToken).ConfigureAwait(false);
+        return pipeline?.ToDataPipelineStatus();
+    }
+
+    /// <inheritdoc />
+    public Task<SearchResult> SearchAsync(
+        string query,
+        string? index = null,
+        MemoryFilter? filter = null,
+        ICollection<MemoryFilter>? filters = null,
+        double minRelevance = 0,
+        int limit = -1,
+        CancellationToken cancellationToken = default)
+    {
+        if (filter != null)
+        {
+            if (filters == null) { filters = new List<MemoryFilter>(); }
+
+            filters.Add(filter);
+        }
+
+        index = IndexExtensions.CleanName(index);
+        return this._searchClient.SearchAsync(
+            index: index,
+            query: query,
+            filters: filters,
+            minRelevance: minRelevance,
+            limit: limit,
+            cancellationToken: cancellationToken);
+    }
+
+    /// <inheritdoc />
+    public Task<MemoryAnswer> AskAsync(
+        string question,
+        string? index = null,
+        MemoryFilter? filter = null,
+        ICollection<MemoryFilter>? filters = null,
+        double minRelevance = 0,
+        CancellationToken cancellationToken = default)
+    {
+        if (filter != null)
+        {
+            if (filters == null) { filters = new List<MemoryFilter>(); }
+
+            filters.Add(filter);
+        }
+
+        index = IndexExtensions.CleanName(index);
+        return this._searchClient.AskAsync(
+            index: index,
+            question: question,
+            filters: filters,
+            minRelevance: minRelevance,
+            cancellationToken: cancellationToken);
+    }
+}
