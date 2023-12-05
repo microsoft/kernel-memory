e2601258352a254e025f61f405a64913c7c1855c
Devis Lucato <devis@microsoft.com>
2023-12-05 15:28:13 -0800

    Update AI clients

---
+++ examples/002-dotnet-SemanticKernelPlugin/KernelFunctionExtensions.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel;
+
+namespace Microsoft.KernelMemory.SKExtensions;
+
+public static class KernelFunctionExtensions
+{
+    public static Task<FunctionResult> InvokeWithTextAsync(
+        this KernelFunction function,
+        Kernel kernel,
+        string? text,
+        CancellationToken cancellationToken = default)
+    {
+        var args = new KernelArguments { ["input"] = text };
+        return function.InvokeAsync(kernel, args, cancellationToken);
+    }
+}
