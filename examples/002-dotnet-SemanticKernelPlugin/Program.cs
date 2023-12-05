a594b05ae41f6cd0df0a51de6732851e7e8328cd
Devis Lucato <devis@microsoft.com>
2023-12-05 12:09:06 -0800

    Upgrade Azure.Search.Documents to 11.5.1 Upgrade example 002 to SK rc1 Other minor upgrades

--- examples/002-dotnet-SemanticKernelPlugin/Program.cs
+++ examples/002-dotnet-SemanticKernelPlugin/Program.cs
@@ -2,7 +2,6 @@
 
 using Microsoft.KernelMemory;
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Orchestration;
 
 public static class Program
 {
@@ -20,15 +19,18 @@ public static class Program
 
         var builder = new KernelBuilder();
         builder
-            // .WithOpenAIChatCompletionService(
+            // For OpenAI:
+            // .WithOpenAIChatCompletion(
             //     modelId: "gpt-3.5-turbo",
             //     apiKey: EnvVar("OPENAI_API_KEY"))
-            .WithAzureOpenAIChatCompletionService(
+            // Azure OpenAI:
+            .WithAzureOpenAIChatCompletion(
                 deploymentName: EnvVar("AOAI_DEPLOYMENT_TEXT"),
+                modelId: EnvVar("AOAI_DEPLOYMENT_TEXT"),
                 endpoint: EnvVar("AOAI_ENDPOINT"),
                 apiKey: EnvVar("AOAI_API_KEY"));
 
-        var kernel = builder.Build();
+        Kernel kernel = builder.Build();
 
         // === PREPARE A SIMPLE SEMANTIC FUNCTION
         // A simple prompt showing how you can leverage the memory inside prompts and semantic functions.
@@ -43,7 +45,7 @@ public static class Program
                        If the answer is empty say 'I don't know' otherwise reply with a preview of the answer, truncated to 15 words.
                        """;
 
-        var oracleFunction = kernel.CreateSemanticFunction(skPrompt);
+        var oracleFunction = kernel.CreateFunctionFromPrompt(skPrompt);
 
         // === PREPARE MEMORY PLUGIN ===
         // Load the Kernel Memory plugin into Semantic Kernel.
@@ -51,7 +53,7 @@ public static class Program
         // otherwise change the URL pointing to your KM endpoint.
 
         var memoryConnector = GetMemoryConnector();
-        var memoryPlugin = kernel.ImportFunctions(new MemoryPlugin(memoryConnector, waitForIngestionToComplete: true), "memory");
+        var memoryPlugin = kernel.ImportPluginFromObject(new MemoryPlugin(memoryConnector, waitForIngestionToComplete: true), "memory");
 
         // === LOAD DOCUMENT INTO MEMORY ===
         // Load some data in memory, in this case use a PDF file, though
@@ -59,10 +61,12 @@ public static class Program
 
         // You can use either the plugin or the connector, the result is the same
         // await memoryConnector.ImportDocumentAsync(filePath: DocFilename, documentId: "NASA001");
-        var context = kernel.CreateNewContext();
-        context.Variables[MemoryPlugin.FilePathParam] = DocFilename;
-        context.Variables[MemoryPlugin.DocumentIdParam] = "NASA001";
-        await memoryPlugin["SaveFile"].InvokeAsync(context);
+        var context = new KernelArguments
+        {
+            [MemoryPlugin.FilePathParam] = DocFilename,
+            [MemoryPlugin.DocumentIdParam] = "NASA001"
+        };
+        await memoryPlugin["SaveFile"].InvokeAsync(kernel, context);
 
         // === RUN SEMANTIC FUNCTION ===
         // Run some example questions, showing how the answer is grounded on the document uploaded.
@@ -71,17 +75,17 @@ public static class Program
 
         Console.WriteLine("---------");
         Console.WriteLine(Question1 + " (expected: some answer using the PDF provided)\n");
-        FunctionResult answer = await oracleFunction.InvokeAsync(Question1, kernel);
+        FunctionResult answer = await oracleFunction.InvokeAsync(kernel, Question1);
         Console.WriteLine("Answer: " + answer);
 
         Console.WriteLine("---------");
         Console.WriteLine(Question2 + " (expected answer: \"I don't know\")\n");
-        answer = await oracleFunction.InvokeAsync(Question2, kernel);
+        answer = await oracleFunction.InvokeAsync(kernel, Question2);
         Console.WriteLine("Answer: " + answer);
 
         Console.WriteLine("---------");
         Console.WriteLine(Question3 + " (expected answer: \"I don't know\")\n");
-        answer = await oracleFunction.InvokeAsync(Question3, kernel);
+        answer = await oracleFunction.InvokeAsync(kernel, Question3);
         Console.WriteLine("Answer: " + answer);
     }
 
