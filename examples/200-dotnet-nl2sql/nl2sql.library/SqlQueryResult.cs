9e3072ca862f250be167c38473221af6fb5be4ac
Devis Lucato <devis@microsoft.com>
2024-01-16 14:24:00 -0800

    Upgrade Service to use latest KM nugets Upgrade KM components to latest SK build

--- examples/200-dotnet-nl2sql/nl2sql.library/SqlQueryResult.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-﻿
-namespace SemanticKernel.Data.Nl2Sql.Library;
-
-public record SqlQueryResult(string Schema, string Query)
-{
-    // No specialization
-}
--- examples/200-dotnet-nl2sql/run.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-setlocal
-
-cd %~dp0/nl2sql.console
-
-dotnet run
