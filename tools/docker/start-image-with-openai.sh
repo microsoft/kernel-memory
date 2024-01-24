5862728608d39a282841e1d25fe9d9a28ca69796
Devis Lucato <devis@microsoft.com>
2024-01-23 22:15:12 -0800

    .

--- tools/docker/start-image-with-openai.sh
+++ tools/docker/start-image-with-openai.sh
@@ -3,7 +3,7 @@
 # Use this script to start the Docker image without a config file, with a basic setup using OpenAI default settings
 
 if [ -z "$OPENAI_API_KEY" ]; then
-    echo "The variable OPENAI_API_KEY is not set"
+    echo "The OPENAI_API_KEY environment variable is not set"
     exit 1
 fi
 
@@ -13,7 +13,3 @@ ROOT="$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" && cd ../.. && pwd)"
 cd $ROOT
 
 docker run -it --rm --name kernelmemory -e OPENAI_DEMO="${OPENAI_API_KEY}" kernel-memory/service:latest
-
-
-# To inspect the image contenta after starting it:
-#   docker exec -it kernelmemory /bin/sh
