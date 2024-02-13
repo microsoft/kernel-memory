3c9281334f57cf9ded20a0901ecc93693d752bae
Devis Lucato <dluc@users.noreply.github.com>
2024-02-13 11:42:46 -0800

    Update start-image.sh

--- tools/docker/start-image.sh
+++ tools/docker/start-image.sh
@@ -7,11 +7,13 @@ set -e
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" && cd ../.. && pwd)"
 cd $ROOT
 
-docker run -it --rm --name kernelmemory -v ./service/Service/appsettings.Development.json:/app/data/appsettings.json kernelmemory/service
+docker run -it --rm --name kernelmemory \
+    -v ./service/Service/appsettings.Development.json:/app/data/appsettings.Production.json \
+    kernelmemory/service
 
 
 
 # To inspect the image content after starting it: docker exec -it kernelmemory /bin/sh
-# ... or before starting it: docker run -it --rm --name kernelmemory -v ./service/Service/appsettings.Development.json:/app/data/appsettings.json --entrypoint /bin/sh kernelmemory/service:latest
+# ... or before starting it: docker run -it --rm --name kernelmemory -v ./service/Service/appsettings.Development.json:/app/data/appsettings.Production.json --entrypoint /bin/sh kernelmemory/service:latest
 
 
