372dee5eb1ddde824cda04f662f9156d8ba66525
Devis Lucato <devis@microsoft.com>
2024-03-13 17:00:38 +0100

    Code style. Keep old signatures for maintainability.

--- service/Core/Handlers/DependencyInjection.cs
+++ service/Core/Handlers/DependencyInjection.cs
@@ -103,11 +103,11 @@ public static partial class DependencyInjection
         // Build generic type: HandlerAsAHostedService<THandler>
         Type handlerAsAHostedServiceTHandler = typeof(HandlerAsAHostedService<>).MakeGenericType(tHandler);
 
-        IHostedService implementationFactory(IServiceProvider serviceProvider)
+        IHostedService ImplementationFactory(IServiceProvider serviceProvider)
             => (IHostedService)ActivatorUtilities.CreateInstance(serviceProvider, handlerAsAHostedServiceTHandler, stepName);
 
         // See https://github.com/dotnet/runtime/issues/38751 for troubleshooting
-        services.Add(ServiceDescriptor.Singleton<IHostedService>(implementationFactory));
+        services.Add(ServiceDescriptor.Singleton<IHostedService>((Func<IServiceProvider, IHostedService>)ImplementationFactory));
 
         return services;
     }
