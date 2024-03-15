c0ce07ab49845a166f33e2cd5fae080346b31fea
Devis Lucato <dluc@users.noreply.github.com>
2024-03-15 05:09:05 -0700

    Update tools/InteractiveSetup/Main.cs

--- tools/InteractiveSetup/Main.cs
+++ tools/InteractiveSetup/Main.cs
@@ -628,8 +628,7 @@ public static class Main
 
         AppSettings.Change(x => x.Services[ServiceName] = new Dictionary<string, object>
         {
-            { "StorageType", string.IsNullOrEmpty(queueFolderOnDisk) ? "Volatile" : "Disk" },
-            { "Directory", queueFolderOnDisk }
+            { "Directory", SetupUI.AskOpenQuestion("Directory where to store queue messages", config["Directory"].ToString()) }
         });
     }
 
