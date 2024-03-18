7f72ce4457137cd9858f7425dae98d846ec5c2c6
xbotter <xbotter@users.noreply.github.com>
2024-03-18 18:35:04 +0800

    Using Semantic Kernel ITextCompletion As ITextGeneration (#183)
    ## Motivation and Context (Why the change? What's the scenario?)
    
    This seems somewhat recursive, to use Semantic Kernel as the
    infrastructure for Kernel Memory. However, the interfaces of both are
    very similar. So, I added an adapter that can directly use SK's
    ITextCompletion to maximize the reuse of LLM adapted for SK.

---
+++ service/Core/SemanticKernel/KernelMemoryBuilderExtensions.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.Extensions.Logging;
+using Microsoft.KernelMemory.AI;
+using Microsoft.KernelMemory.Configuration;
+using Microsoft.KernelMemory.SemanticKernel;
+using Microsoft.SemanticKernel.Embeddings;
+using Microsoft.SemanticKernel.TextGeneration;
+
+#pragma warning disable IDE0130 // reduce number of "using" statements
+// ReSharper disable once CheckNamespace - reduce number of "using" statements
+namespace Microsoft.KernelMemory;
+
+/// <summary>
+/// Kernel Memory builder extensions.
+/// </summary>
+public static partial class KernelMemoryBuilderExtensions
+{
+    /// <summary>
+    /// Inject an implementation of <see cref="ITextGenerationService">SK text generation service</see>
+    /// for local dependencies on <see cref="ITextGenerator"/>
+    /// </summary>
+    /// <param name="builder">KM builder</param>
+    /// <param name="service">SK text generation service instance</param>
+    /// <param name="config">SK text generator settings</param>
+    /// <param name="tokenizer">Tokenizer used to count tokens used by prompts</param>
+    ///  <param name="loggerFactory">.NET logger factory</param>
+    /// <returns>KM builder</returns>
+    public static IKernelMemoryBuilder WithSemanticKernelTextGenerationService(
+        this IKernelMemoryBuilder builder,
+        ITextGenerationService service,
+        SemanticKernelConfig config,
+        ITextTokenizer? tokenizer = null,
+        ILoggerFactory? loggerFactory = null)
+    {
+        if (service == null) { throw new ConfigurationException("The semantic kernel text generation service instance is NULL"); }
+
+        return builder.AddSingleton<ITextGenerator>(new SemanticKernelTextGenerator(service, config, tokenizer, loggerFactory));
+    }
+
+    ///  <summary>
+    /// Inject an implementation of<see cref="ITextEmbeddingGenerationService">SK text embedding generation service</see>
+    ///  for local dependencies on <see cref="ITextEmbeddingGenerator"/>
+    ///  </summary>
+    ///  <param name="builder">KM builder</param>
+    ///  <param name="service">SK text embedding generation instance</param>
+    ///  <param name="config">SK text embedding generator settings</param>
+    ///  <param name="tokenizer">Tokenizer used to count tokens sent to the embedding generator</param>
+    ///  <param name="loggerFactory">.NET logger factory</param>
+    ///  <param name="onlyForRetrieval">Whether to use this embedding generator only during data ingestion, and not for retrieval (search and ask API)</param>
+    ///  <returns>KM builder</returns>
+    public static IKernelMemoryBuilder WithSemanticKernelTextEmbeddingGenerationService(
+        this IKernelMemoryBuilder builder,
+        ITextEmbeddingGenerationService service,
+        SemanticKernelConfig config,
+        ITextTokenizer? tokenizer = null,
+        ILoggerFactory? loggerFactory = null,
+        bool onlyForRetrieval = false)
+    {
+        if (service == null) { throw new ConfigurationException("The semantic kernel text embedding generation service instance is NULL"); }
+
+        var generator = new SemanticKernelTextEmbeddingGenerator(service, config, tokenizer, loggerFactory);
+        builder.AddSingleton<ITextEmbeddingGenerator>(generator);
+
+        if (!onlyForRetrieval)
+        {
+            builder.AddIngestionEmbeddingGenerator(generator);
+        }
+
+        return builder;
+    }
+}
---
+++ service/Core/SemanticKernel/SemanticKernelConfig.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace Microsoft.KernelMemory.SemanticKernel;
+
+/// <summary>
+/// Semantic Kernel TextGenerator And TextEmbeddingGenerator Config
+/// </summary>
+public class SemanticKernelConfig
+{
+    /// <summary>
+    /// Max size of the LLM attention window, ie max tokens that can be processed.
+    /// </summary>
+    public int MaxTokenTotal { get; set; } = 8191;
+}
