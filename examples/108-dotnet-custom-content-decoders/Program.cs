a5ca3e8150851fa0c045f6a67449fde3f6a8dfbd
Marco Minerva <marco.minerva@gmail.com>
2024-04-10 14:55:14 +0200

    Add sample for custom content decoders

---
+++ examples/108-dotnet-custom-content-decoders/Program.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.KernelMemory;
+
+var azureOpenAITextConfig = new AzureOpenAIConfig();
+var azureOpenAIEmbeddingConfig = new AzureOpenAIConfig();
+
+new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile("appsettings.Development.json", optional: true)
+    .Build()
+    .BindSection("KernelMemory:Services:AzureOpenAIText", azureOpenAITextConfig)
+    .BindSection("KernelMemory:Services:AzureOpenAIEmbedding", azureOpenAIEmbeddingConfig);
+
+var memory = new KernelMemoryBuilder()
+    .WithAzureOpenAITextGeneration(azureOpenAITextConfig)
+    .WithAzureOpenAITextEmbeddingGeneration(azureOpenAIEmbeddingConfig)
+    .Build<MemoryServerless>();
+
+await memory.ImportDocumentAsync("file5-NASA-news.pdf");
