8eef81adb091b4a0230ae63172654230e3340f23
Devis Lucato <devis@microsoft.com>
2024-04-17 21:28:19 -0700

    Experimental Discord connector

---
+++ extensions/Discord/Discord.TestApplication/Program.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.KernelMemory;
+using Microsoft.KernelMemory.ContentStorage.DevTools;
+using Microsoft.KernelMemory.Sources.DiscordBot;
+
+namespace Microsoft.Discord.TestApplication;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        WebApplicationBuilder appBuilder = WebApplication.CreateBuilder();
+
+        var appSettings = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile("appsettings.Development.json", optional: true)
+            .Build();
+
+        var discordConfig = new DiscordConnectorConfig();
+        appSettings.GetSection("Discord").Bind(discordConfig);
+        appBuilder.Services.AddSingleton(discordConfig);
+        appBuilder.Services.AddHostedService<DiscordConnector>();
+
+        // var app = BuildAsynchronousApp(appBuilder, discordConfig);
+        var app = BuildSynchronousApp(appBuilder, discordConfig);
+
+        Console.WriteLine("Starting application...");
+        app.Run();
+        Console.WriteLine("... application stopped.");
+    }
+
+    private static WebApplication BuildAsynchronousApp(WebApplicationBuilder appBuilder, DiscordConnectorConfig discordConfig)
+    {
+        appBuilder.Services.AddHandlerAsHostedService<DiscordMessageHandler>(discordConfig.Steps[0]);
+        appBuilder.AddKernelMemory(kmb =>
+        {
+            // Note: because of this the memory instance will be asynchronous (ie MemoryService)
+            kmb.WithSimpleQueuesPipeline();
+
+            // Store files on disk
+            kmb.WithSimpleFileStorage(SimpleFileStorageConfig.Persistent);
+
+            // Required internally, won't be used
+            kmb.WithOpenAIDefaults("no key");
+        });
+
+        return appBuilder.Build();
+    }
+
+    private static WebApplication BuildSynchronousApp(WebApplicationBuilder appBuilder, DiscordConnectorConfig discordConfig)
+    {
+        appBuilder.AddKernelMemory(kmb =>
+        {
+            // Note: there's no queue system, so the memory instance will be synchronous (ie MemoryServerless)
+
+            // Store files on disk
+            kmb.WithSimpleFileStorage(SimpleFileStorageConfig.Persistent);
+
+            // Required internally, won't be used
+            kmb.WithOpenAIDefaults("no key");
+        });
+
+        WebApplication app = appBuilder.Build();
+
+        // In synchronous apps, handlers are added to the serverless memory orchestrator
+        (app.Services.GetService<IKernelMemory>() as MemoryServerless)!
+            .Orchestrator
+            .AddHandler<DiscordMessageHandler>(discordConfig.Steps[0]);
+
+        return app;
+    }
+}
---
+++ extensions/Discord/Discord/DiscordMessageHandler.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.KernelMemory.Pipeline;
+
+namespace Microsoft.KernelMemory.Sources.DiscordBot;
+
+public sealed class DiscordMessageHandler : IPipelineStepHandler
+{
+    public string StepName { get; } = string.Empty;
+
+    public DiscordMessageHandler(string stepName)
+    {
+        this.StepName = stepName;
+    }
+
+    public Task<(bool success, DataPipeline updatedPipeline)> InvokeAsync(DataPipeline pipeline, CancellationToken cancellationToken = default)
+    {
+        Console.WriteLine("## DISCORD FILE RECEIVED ##");
+        return Task.FromResult((true, pipeline));
+    }
+}
