f504c8ad5ae2bcafe875a307ef75526e85914259
Devis Lucato <devis@microsoft.com>
2024-04-24 16:19:26 -0700

    Add new IndexNotFound exception

---
+++ service/Abstractions/MemoryStorage/IndexNotFound.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+
+namespace Microsoft.KernelMemory.MemoryStorage;
+
+public class IndexNotFound : KernelMemoryException
+{
+    /// <inheritdoc />
+    public IndexNotFound() { }
+
+    /// <inheritdoc />
+    public IndexNotFound(string message) : base(message) { }
+
+    /// <inheritdoc />
+    public IndexNotFound(string message, Exception? innerException) : base(message, innerException) { }
+}
