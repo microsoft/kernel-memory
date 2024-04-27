61682f49c2424286d83284fd3a066c5e70d455fd
Devis Lucato <devis@microsoft.com>
2024-04-26 19:17:45 -0700

    New TikToken tokenizers package

---
+++ extensions/TikToken/README.md
@@ -0,0 +1,7 @@
+# Kernel Memory with TikToken tokenization
+
+[![Discord](https://img.shields.io/discord/1063152441819942922?label=Discord&logo=discord&logoColor=white&color=d82679)](https://aka.ms/KMdiscord)
+
+This project contains
+the [TikToken](https://github.com/openai/tiktoken)
+tokenizer for Kernel Memory.
---
+++ extensions/TikToken/TikToken/TikTokenGPT2Tokenizer.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.ML.Tokenizers;
+
+namespace Microsoft.KernelMemory.AI.TikToken;
+
+/// <summary>
+/// TikToken GPT2 tokenizer (gpt2.tiktoken)
+/// </summary>
+public class TikTokenGPT2Tokenizer : ITextTokenizer
+{
+    private static readonly Tokenizer s_tokenizer = Tokenizer.CreateTiktokenForModel("gpt2");
+
+    /// <inheritdoc />
+    public int CountTokens(string text)
+    {
+        return s_tokenizer.CountTokens(text);
+    }
+}
