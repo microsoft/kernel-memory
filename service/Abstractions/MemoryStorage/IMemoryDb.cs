3bd177e8e3d0af82b24041b4253949f9653eaa09
Devis Lucato <devis@microsoft.com>
2024-04-26 19:30:54 -0700

    New+Optional memorydb interface to support batch memories upset

--- service/Abstractions/MemoryStorage/IMemoryDb.cs
+++ service/Abstractions/MemoryStorage/IMemoryDb.cs
@@ -39,7 +39,7 @@ public interface IMemoryDb
         CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// Insert/Update a vector + payload, creates the given index if it does not exist.
+    /// Insert/Update a vector + payload.
     /// </summary>
     /// <param name="index">Index/Collection name</param>
     /// <param name="record">Vector + payload to save</param>
