3a97129181475d06ff2eed317d695c8f1fec66d2
Devis Lucato <devis@microsoft.com>
2024-05-08 11:09:58 -0700

    Discord connector + test app

---
+++ extensions/Discord/Discord/DiscordConnectorConfig.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+
+namespace Microsoft.KernelMemory.Sources.DiscordBot;
+
+/// <summary>
+/// Discord bot settings
+/// </summary>
+public class DiscordConnectorConfig
+{
+    /// <summary>
+    /// Discord bot authentication token
+    /// </summary>
+    public string DiscordToken { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Index where to store files (not memories)
+    /// </summary>
+    public string Index { get; set; } = "discord";
+
+    /// <summary>
+    /// File name used when uploading a message.
+    /// </summary>
+    public string FileName { get; set; } = "discord.json";
+
+    /// <summary>
+    /// Handlers processing the incoming Discord events
+    /// </summary>
+    public List<string> Steps { get; set; } = [];
+}
