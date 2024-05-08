ef0ba2a3480eaf68bef05547d254721021cb8696
Alessandro Federici <afederici75@users.noreply.github.com>
2024-05-08 01:42:25 -0400

    Port Elasticsearch connector to KM repo (#465)
    ## Motivation and Context (Why the change? What's the scenario?)
    
    @dluc here's a PR that uses the latest main with the changes to the
    nuget packages.
    Everything builds and the Elasticsearch tests work, too. I only included
    the unittest that descends from BaseFunctionalTestCase for now. I will
    include the others once I see if this is good so far.
    
    ## High level description (Approach, Design)

---
+++ extensions/Elasticsearch/Elasticsearch/DependencyInjection.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) Free Mind Labs, Inc. All rights reserved.
+
+using Microsoft.Extensions.DependencyInjection;
+using Elastic.Clients.Elasticsearch;
+using Microsoft.KernelMemory.MemoryStorage;
+using Microsoft.KernelMemory.MemoryDb.Elasticsearch;
+
+namespace Microsoft.KernelMemory;
+
+/// <summary>
+/// DI pipelines for Elasticsearch Memory.
+/// </summary>
+public static partial class KernelMemoryBuilderExtensions
+{
+    /// <summary>
+    /// Kernel Memory Builder extension method to add the Elasticsearch memory connector.
+    /// </summary>
+    /// <param name="builder">The IKernelMemoryBuilder instance</param>
+    /// <param name="configuration">The application configuration</param>"
+    public static IKernelMemoryBuilder WithElasticsearch(this IKernelMemoryBuilder builder,
+        ElasticsearchConfig configuration)
+    {
+        builder.Services.AddElasticsearchAsMemoryDb(configuration);
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Extension method to add the Elasticsearch memory connector.
+    /// </summary>
+    /// <param name="builder"></param>
+    /// <param name="configure"></param>
+    /// <returns></returns>
+    public static IKernelMemoryBuilder WithElasticsearch(this IKernelMemoryBuilder builder,
+        Action<ElasticsearchConfigBuilder> configure)
+    {
+        ArgumentNullException.ThrowIfNull(configure, nameof(configure));
+
+        var cfg = new ElasticsearchConfigBuilder();
+        configure(cfg);
+
+        builder.Services.AddElasticsearchAsMemoryDb(cfg.Build());
+        return builder;
+    }
+}
+
+/// <summary>
+/// Setup Elasticsearch memory within the semantic kernel.
+/// </summary>
+public static partial class DependencyInjection
+{
+    /// <summary>
+    /// Inject Elasticsearch as the default implementation of IMemoryDb
+    /// </summary>
+    public static IServiceCollection AddElasticsearchAsMemoryDb(this IServiceCollection services,
+        ElasticsearchConfig esConfig)
+    {
+        ArgumentNullException.ThrowIfNull(esConfig, nameof(esConfig));
+
+        // The ElasticsearchClient type is thread-safe and can be shared and
+        // reused across multiple threads in consuming applications. 
+        // See https://www.elastic.co/guide/en/elasticsearch/client/net-api/current/recommendations.html
+        services.AddSingleton(sp =>
+        {
+            var esConfig = sp.GetRequiredService<ElasticsearchConfig>();
+            return new ElasticsearchClient(esConfig.ToElasticsearchClientSettings());
+        });
+
+        return services
+            .AddSingleton<IIndexNameHelper, IndexNameHelper>()
+            .AddSingleton(esConfig)
+            .AddSingleton<IMemoryDb, ElasticsearchMemory>();
+    }
+
+    /// <summary>
+    /// Inject Elasticsearch as the default implementation of IMemoryDb
+    /// </summary>
+    public static IServiceCollection AddElasticsearchAsMemoryDb(this IServiceCollection services,
+               Action<ElasticsearchConfigBuilder> configure)
+    {
+        ArgumentNullException.ThrowIfNull(configure, nameof(configure));
+
+        var cfg = new ElasticsearchConfigBuilder();
+        configure(cfg);
+
+        return services.AddElasticsearchAsMemoryDb(cfg.Build());
+    }
+}
---
+++ extensions/Elasticsearch/Elasticsearch/Internals/Exceptions.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Free Mind Labs, Inc. All rights reserved.
+
+using Microsoft.KernelMemory;
+
+namespace Microsoft.KernelMemory.MemoryDb.Elasticsearch;
+
+/// <summary>
+/// Base exception for all the exceptions thrown by the Elasticsearch connector for KernelMemory
+/// </summary>
+public class ElasticsearchException : KernelMemoryException
+{
+    /// <inheritdoc />
+    public ElasticsearchException() { }
+
+    /// <inheritdoc />
+    public ElasticsearchException(string message) : base(message) { }
+
+    /// <inheritdoc />
+    public ElasticsearchException(string message, Exception? innerException) : base(message, innerException) { }
+}
+
+/// <summary>
+/// Exception thrown when the Elasticsearch configuration is invalid in appSettings, secrets, etc.
+/// </summary>
+public class ConfigurationException : ElasticsearchException
+{
+    /// <inheritdoc />
+    public ConfigurationException() { }
+
+    /// <inheritdoc />
+    public ConfigurationException(string message) : base(message) { }
+
+    /// <inheritdoc />
+    public ConfigurationException(string message, Exception? innerException) : base(message, innerException) { }
+}
+
+/// <summary>
+/// Exception thrown when an index name does pass Elasticsearch validation.
+/// </summary>
+public class InvalidIndexNameException : ElasticsearchException
+{
+    /// <inheritdoc/>
+    public InvalidIndexNameException(string indexName, IEnumerable<string> errors, Exception? innerException = default)
+        : base($"The given index name '{indexName}' is invalid. {string.Join(", ", errors)}", innerException)
+    {
+        this.IndexName = indexName;
+        this.Errors = errors;
+    }
+
+    /// <inheritdoc/>
+    public InvalidIndexNameException(
+        (string IndexName, IEnumerable<string> Errors) conversionResult,
+        Exception? innerException = default)
+
+        => (this.IndexName, this.Errors) = conversionResult;
+
+    /// <summary>
+    /// The index name that failed validation.
+    /// </summary>
+    public string IndexName { get; }
+
+    /// <summary>
+    /// The list of errors that caused the validation to fail.
+    /// </summary>
+    public IEnumerable<string> Errors { get; }
+}
+
