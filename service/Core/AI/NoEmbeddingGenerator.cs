dcd2a0e9c5dfeb1f896f746f21272673f71a4109
Devis Lucato <dluc@users.noreply.github.com>
2024-05-09 15:36:44 -0700

    Add DI helpers for scenarios not requiring AI (#472)
    Kernel Memory can be utilized to create applications that don't require
    Text Generation or Embedding Generation. However, these dependencies
    must still be set up. Given the absence of a method that is
    simultaneously fast, free, and of high quality for this setup —
    especially for embeddings — without resorting to API calls and
    authentication keys, the Kernel Memory builder lacks a default
    configuration and throws an exception (see issue #466 for an example).
    
    To address this, we introduce two straightforward solutions that allow
    the injection of placeholder text and embedding generators:
    
    * `KernelMemoryBuilder.WithoutEmbeddingGenerator()`
    * `KernelMemoryBuilder.WithoutTextGenerator()`
    
    Alternatively:
    
    * `appBuidler.Services.AddNoEmbeddingGenerator()`
    * `appBuidler.Services.AddNoTextGenerator()`
    
    Should these placeholder classes be called upon to generate text or
    embeddings, they will intentionally throw a `NotImplementedException`
    exception. This ensures that any overlooked dependencies can be
    identified.

---
+++ service/Core/AI/NoEmbeddingGenerator.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.KernelMemory.AI;
+
+/// <summary>
+/// Disabled embedding generator used when using KM without embeddings,
+/// e.g. when using the internal orchestration to run jobs that don't require AI.
+/// </summary>
+public class NoEmbeddingGenerator : ITextEmbeddingGenerator
+{
+    private readonly ILogger<ITextEmbeddingGenerator> _log;
+
+    public NoEmbeddingGenerator(ILoggerFactory loggerFactory)
+    {
+        this._log = loggerFactory.CreateLogger<ITextEmbeddingGenerator>();
+    }
+
+    /// <inheritdoc />
+    public int MaxTokens => int.MaxValue;
+
+    /// <inheritdoc />
+    public int CountTokens(string text)
+    {
+        throw this.Error();
+    }
+
+    /// <inheritdoc />
+    public Task<Embedding> GenerateEmbeddingAsync(string text, CancellationToken cancellationToken = default)
+    {
+        throw this.Error();
+    }
+
+    private NotImplementedException Error()
+    {
+        this._log.LogCritical("The application is attempting to generate embeddings even if embedding generation has been disabled");
+        return new NotImplementedException("Embedding generation has been disabled");
+    }
+}
