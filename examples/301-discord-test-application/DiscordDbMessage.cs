7c543c46dd8fe0b5a9ba90a61a8320f49413adbe
Devis Lucato <dluc@users.noreply.github.com>
2024-05-09 22:27:26 -0700

    Experimental Discord connector (#463)
    ## Motivation and Context (Why the change? What's the scenario?)
    
    Allow ingesting Discord messages in memory, to populate memory and
    trigger AI-driven actions.
    
    Add example, including handler and code to store Discord messages in
    Postgres.
    
    ## High level description (Approach, Design)
    
    * Add connector based on DiscordBod.NET to fetch messages. The Discord
    bot can be added to multiple servers.
    * Whenever a message is received serialize the message data and trigger
    a memory pipeline, uploading the file to content storage.
    * Pipeline steps are configurable, in order to process the serialized
    data with custom logic before doing anything else.

---
+++ examples/301-discord-test-application/DiscordDbMessage.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.ComponentModel.DataAnnotations;
+using Microsoft.KernelMemory.Sources.DiscordBot;
+
+namespace Microsoft.Discord.TestApplication;
+
+public class DiscordDbMessage : DiscordMessage
+{
+    [Key]
+    public string Id
+    {
+        get
+        {
+            return this.MessageId;
+        }
+        set
+        {
+            this.MessageId = value;
+        }
+    }
+}
