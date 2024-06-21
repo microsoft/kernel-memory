78edf56fc9fdafbefb01c83bca72bcf623e43150
Devis Lucato <devis@microsoft.com>
2024-06-20 23:01:21 -0700

    Fix InteractiveSetup

--- tools/InteractiveSetup/Services/AWSS3.cs
+++ tools/InteractiveSetup/Services/AWSS3.cs
@@ -19,19 +19,23 @@ internal static class AWSS3
             config = new Dictionary<string, object>
             {
                 { "Auth", "AccessKey" },
-                { "Account", "" },
-                { "Container", "kmemory" },
-                { "ConnectionString", "" },
+                { "AccessKey", "" },
+                { "SecretAccessKey", "" },
+                { "BucketName", "" },
+                { "Endpoint", "https://s3.amazonaws.com" },
             };
         }
+
+        // Required to avoid exceptions. "Endpoint" is optional and not defined in appsettings.json
+        config.TryAdd("Endpoint", "https://s3.amazonaws.com");
 
         AppSettings.Change(x => x.Services[ServiceName] = new Dictionary<string, object>
         {
             { "Auth", "AccessKey" },
             { "AccessKey", SetupUI.AskOpenQuestion("AWS S3 <access key>", config["AccessKey"].ToString()) },
             { "SecretAccessKey", SetupUI.AskPassword("AWS S3 <secret access key>", config["SecretAccessKey"].ToString()) },
-            { "Endpoint", SetupUI.AskOpenQuestion("AWS S3 <endpoint>", config["Endpoint"].ToString()) },
             { "BucketName", SetupUI.AskOpenQuestion("AWS S3 <bucket name>", config["BucketName"].ToString()) },
+            { "Endpoint", SetupUI.AskOpenQuestion("AWS S3 <endpoint>", config["Endpoint"].ToString()) },
         });
     }
 }
