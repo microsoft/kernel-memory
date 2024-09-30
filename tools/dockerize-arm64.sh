7a429f3221cbfe4f5f5285a986c5023e2152630b
Devis Lucato <dluc@users.noreply.github.com>
2024-09-30 13:50:14 -0700

    Fixing dockerization for ARM64 (#814)
    * Remove multi-arch builds, which are not working correctly on ARM
    (segmentation fault)
    * Split github workflow by arch. ARM arch dockerization to be run
    manually for now, until fixed.
    * Deprecate "sha" labels to avoid confusion)

---
+++ tools/dockerize-arm64.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/"
+cd "$HERE"
+
+USR=kernelmemory
+IMG=${USR}/service
+TAG=:latest-arm64
+
+set +e
+docker rmi ${IMG}${TAG} >/dev/null 2>&1
+set -e
+
+if [ -z "$(docker images -q ${IMG}${TAG})" ]; then
+  echo "All ${IMG}${TAG} images have been deleted."
+else
+  echo "Some ${IMG}${TAG} images are still present:"
+  docker images ${IMG}${TAG}
+  exit -1
+fi
+
+# See https://github.com/dotnet/dotnet-docker/blob/main/README.sdk.md#full-tag-listing
+docker buildx build --no-cache --load \
+    --platform=linux/arm64 \
+    --build-arg BUILD_IMAGE_TAG=8.0-jammy-arm64v8 \
+    --build-arg RUN_IMAGE_TAG=8.0-alpine-arm64v8 \
+    -t ${IMG}${TAG} .
+
+docker login -u ${USR} --password-stdin
+
+docker push ${IMG}${TAG}
