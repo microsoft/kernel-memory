265c4a28192a31b94132411df700a8f5ff0436e7
Devis Lucato <dluc@users.noreply.github.com>
2024-10-18 21:04:53 -0700

    Update README and improve simple/dev deps setup (#846)
    * Revisit README structure
    * Add swagger.json to repo and link to visualize the file
    * Allow to select "persistent" simplequeue/simplestorage/simplememory in
    setup tool

--- tools/InteractiveSetup/Services/SimpleVectorDb.cs
+++ tools/InteractiveSetup/Services/SimpleVectorDb.cs
@@ -9,23 +9,26 @@ internal static class SimpleVectorDb
 {
     public static void Setup(Context ctx, bool force = false)
     {
-        if (!ctx.CfgSimpleVectorDb.Value && !force) { return; }
-
-        ctx.CfgSimpleVectorDb.Value = false;
         const string ServiceName = "SimpleVectorDb";
+
+        if (!ctx.CfgSimpleVectorDbVolatile.Value && !ctx.CfgSimpleVectorDbPersistent.Value) { return; }
+
+        var persistent = ctx.CfgSimpleVectorDbPersistent.Value;
+        ctx.CfgSimpleVectorDbVolatile.Value = false;
+        ctx.CfgSimpleVectorDbPersistent.Value = false;
 
         if (!AppSettings.GetCurrentConfig().Services.TryGetValue(ServiceName, out var config))
         {
             config = new Dictionary<string, object>
             {
                 { "Directory", "" },
-                { "StorageType", "Volatile" }
             };
         }
 
         AppSettings.Change(x => x.Services[ServiceName] = new Dictionary<string, object>
         {
-            { "Directory", SetupUI.AskOpenQuestion("Directory where to store vectors", config["Directory"].ToString()) }
+            { "Directory", SetupUI.AskOpenQuestion("Directory where to store vectors", config["Directory"].ToString()) },
+            { "StorageType", persistent ? "Disk" : "Volatile" }
         });
     }
 }
