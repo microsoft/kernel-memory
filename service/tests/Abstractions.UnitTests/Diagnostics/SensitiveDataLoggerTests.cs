ad3e2412b063ba0b6d7efbdf786a5ac8961fed55
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2024-10-22 11:34:30 +1000

    Unit tests for the SensitiveDataLogger (#853)
    ## Motivation and Context (Why the change? What's the scenario?)
    
    Contributing unit tests for the `SensitiveDataLogger` to cover a
    recently fixed bug and prevent future regression.
    
    ## High level description (Approach, Design)
    
    N/A - unit tests only
    
    ---------
    
    Co-authored-by: Devis Lucato <dluc@users.noreply.github.com>

---
+++ service/tests/Abstractions.UnitTests/Diagnostics/SensitiveDataLoggerTests.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.KernelMemory.Diagnostics;
+
+namespace Microsoft.KM.Abstractions.UnitTests.Diagnostics;
+
+public sealed class SensitiveDataLoggerTests : IDisposable
+{
+    private const string EnvironmentVariableName = "ASPNETCORE_ENVIRONMENT";
+
+    [Fact]
+    public void ItIsDisabledByDefault()
+    {
+        // Assert
+        Assert.False(SensitiveDataLogger.Enabled);
+    }
+
+    [Theory]
+    [Trait("Category", "UnitTest")]
+    [InlineData("development")]
+    [InlineData("Development")]
+    public void ItCanBeEnabledInDevelopmentEnvironment(string environment)
+    {
+        // Arrange
+        Assert.False(SensitiveDataLogger.Enabled);
+        Environment.SetEnvironmentVariable(EnvironmentVariableName, environment);
+
+        // Act
+        SensitiveDataLogger.Enabled = true;
+
+        // Assert
+        Assert.True(SensitiveDataLogger.Enabled);
+    }
+
+    [Theory]
+    [Trait("Category", "UnitTest")]
+    [InlineData("staging")]
+    [InlineData("Staging")]
+    [InlineData("production")]
+    [InlineData("Production")]
+    [InlineData("any")]
+    public void ItCannotBeEnabledInNonDevelopmentEnvironments(string environment)
+    {
+        // Arrange
+        Assert.False(SensitiveDataLogger.Enabled);
+        Environment.SetEnvironmentVariable(EnvironmentVariableName, environment);
+
+        // Act/Assert
+        Assert.Throws<InvalidOperationException>(() => SensitiveDataLogger.Enabled = true);
+    }
+
+    [Theory]
+    [Trait("Category", "UnitTest")]
+    [InlineData("development")]
+    [InlineData("Development")]
+    [InlineData("staging")]
+    [InlineData("Staging")]
+    [InlineData("production")]
+    [InlineData("Production")]
+    [InlineData("any")]
+    public void ItCanBeDisabledInAnyEnvironment(string environment)
+    {
+        // Arrange
+        Assert.False(SensitiveDataLogger.Enabled);
+        Environment.SetEnvironmentVariable(EnvironmentVariableName, environment);
+
+        // Act
+        SensitiveDataLogger.Enabled = false;
+
+        // Assert
+        Assert.False(SensitiveDataLogger.Enabled);
+    }
+
+    public void Dispose()
+    {
+        Environment.SetEnvironmentVariable(EnvironmentVariableName, null);
+        SensitiveDataLogger.Enabled = false;
+    }
+}
