2e3b6a95f4ec8b2808a4fb6dca1eaa815ee382b4
Devis Lucato <devis@microsoft.com>
2024-10-30 10:22:29 -0700

    LlamaSharp embeddings

---
+++ extensions/LlamaSharp/LlamaSharp.FunctionalTests/LlamaSharpTextEmbeddingGeneratorTest.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.KernelMemory;
+using Microsoft.KernelMemory.AI.LlamaSharp;
+using Microsoft.KM.TestHelpers;
+using Xunit.Abstractions;
+
+namespace Microsoft.LlamaSharp.FunctionalTests;
+
+public sealed class LlamaSharpTextEmbeddingGeneratorTest : BaseFunctionalTestCase
+{
+    private readonly LlamaSharpTextEmbeddingGenerator _target;
+
+    public LlamaSharpTextEmbeddingGeneratorTest(
+        IConfiguration cfg,
+        ITestOutputHelper output) : base(cfg, output)
+    {
+        this.LlamaSharpConfig.Validate();
+        this._target = new LlamaSharpTextEmbeddingGenerator(this.LlamaSharpConfig.EmbeddingModel, loggerFactory: null);
+        var modelFilename = this.LlamaSharpConfig.TextModel.ModelPath.Split('/').Last().Split('\\').Last();
+        Console.WriteLine($"Model in use: {modelFilename}");
+    }
+
+    [Fact]
+    [Trait("Category", "LlamaSharp")]
+    public async Task ItGeneratesEmbeddingVectors()
+    {
+        // Act
+        Embedding embedding = await this._target.GenerateEmbeddingAsync("some text");
+
+        // Assert
+        Console.WriteLine("Embedding size: " + embedding.Length);
+
+        // Expected result using nomic-embed-text-v1.5.Q8_0.gguf
+        Assert.Equal(768, embedding.Length);
+    }
+}
