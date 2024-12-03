41d51119f09cddd3e4896f35fcd52c3f35f5f995
Devis Lucato <dluc@users.noreply.github.com>
2024-12-02 18:41:30 -0800

    CodeQL suppressions (#924)

---
+++ service/Core/Diagnostics/CodeQL.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+
+namespace Microsoft.KernelMemory.Diagnostics;
+
+public static class CodeQL
+{
+    /// <summary>
+    /// See https://codeql.github.com/codeql-query-help/csharp/cs-log-forging/
+    /// </summary>
+    public static string? NLF(this string? text)
+    {
+        if (text == null) { return text; }
+
+        return text
+            .Replace("\n", "[char(10)]", StringComparison.Ordinal)
+            .Replace("\r", "[char(13)]", StringComparison.Ordinal);
+    }
+}
