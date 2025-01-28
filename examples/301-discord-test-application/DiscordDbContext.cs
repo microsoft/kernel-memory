255b387e891409e3fecc74174cbf87d88bae8b8e
Devis Lucato <devis@microsoft.com>
2025-01-28 14:19:52 +0100

    Update discord example

--- examples/301-discord-test-application/DiscordDbContext.cs
+++ examples/301-discord-test-application/DiscordDbContext.cs
@@ -8,6 +8,7 @@ public class DiscordDbContext : DbContext
 {
     public DbContextOptions<DiscordDbContext> Options { get; }
 
+    // Table to store Discord messages, table name is "Messages"
     public DbSet<DiscordDbMessage> Messages { get; set; }
 
     public DiscordDbContext(DbContextOptions<DiscordDbContext> options) : base(options)
